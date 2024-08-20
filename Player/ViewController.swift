4cabe1601fec5fdbb2771fcf0904696f046c9477
Hailey <haileykuo96@gmail.com>
2024-08-20 12:19:12 +0800

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by 郭蕙瑄 on 2024/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
